47a759860e327a5e15535a71e538365582197e68
Felipe Schossler <felipealvesschossler@gmail.com>
2023-09-21 11:35:05 -0300

    feat: add more infos movie command

--- cmd/movie/movies.go
+++ cmd/movie/movies.go
@@ -9,13 +9,8 @@ import (
 
 var MovieCmd = &cobra.Command{
 	Use:   "movie",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Some informations about movies",
+	Long:  `Some informations about movies. You can check for top-rated movies, synopsis and details about a movie.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("movie called")
 	},
