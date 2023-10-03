2b81a39f91cb4d3436d7bf81a545b36c3d7a7d38
Felipe Schossler <felipealvesschossler@gmail.com>
2023-10-03 12:06:03 -0300

    feat: add toppopular movie command

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"github.com/fschossler/tmdbcli/cmd"
 	_ "github.com/fschossler/tmdbcli/cmd/movie"
+	_ "github.com/fschossler/tmdbcli/cmd/movie/toppopular"
 	_ "github.com/fschossler/tmdbcli/cmd/movie/toprated"
 	_ "github.com/fschossler/tmdbcli/cmd/serie"
 	_ "github.com/fschossler/tmdbcli/cmd/serie/toprated"
